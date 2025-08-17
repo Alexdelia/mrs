cd841d8b961151645f9c902167661680c4c5ec6c
Alexdelia <alexandre.delille.57@gmail.com>
2025-08-17 11:57:13 +0200

    chore: `rustfmt`

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,22 @@ use crossterm::event::{self, Event};
 use ratatui::{DefaultTerminal, Frame};
 
 fn main() -> Result<()> {
-    color_eyre::install()?;
-    let terminal = ratatui::init();
-    let result = run(terminal);
-    ratatui::restore();
-    result
+	color_eyre::install()?;
+	let terminal = ratatui::init();
+	let result = run(terminal);
+	ratatui::restore();
+	result
 }
 
 fn run(mut terminal: DefaultTerminal) -> Result<()> {
-    loop {
-        terminal.draw(render)?;
-        if matches!(event::read()?, Event::Key(_)) {
-            break Ok(());
-        }
-    }
+	loop {
+		terminal.draw(render)?;
+		if matches!(event::read()?, Event::Key(_)) {
+			break Ok(());
+		}
+	}
 }
 
 fn render(frame: &mut Frame) {
-    frame.render_widget("hello world", frame.area());
+	frame.render_widget("hello world", frame.area());
 }
