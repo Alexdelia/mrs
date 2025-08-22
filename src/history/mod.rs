bd81f0d862899b8f6a02807daa830c6c528dd5e1
Alexdelia <alexandre.delille.57@gmail.com>
2025-08-22 14:04:34 +0200

    add splitting logic with simple test

--- src/history/mod.rs
+++ src/history/mod.rs
@@ -20,19 +20,43 @@ pub struct HistoryRow {
 
 impl HistoryRow {
 	#[inline]
-	fn percentage(value: Float, total: Float) -> Float {
-		if total == 0.0 {
-			0.0
-		} else {
-			(value / total) * 100.0
+	fn split_ratio(&self) -> Float {
+		let total_gain = self.gain1 + self.gain2;
+		if total_gain == 0.0 {
+			return 0.0;
 		}
+		self.rent / total_gain
 	}
 
-	pub fn gain1_percentage(&self) -> Float {
-		Self::percentage(self.gain1, self.rent)
+	pub fn percentage(&self) -> Float {
+		self.split_ratio() * 100.0
 	}
 
-	pub fn gain2_percentage(&self) -> Float {
-		Self::percentage(self.gain2, self.rent)
+	pub fn split_gain1(&self) -> Float {
+		self.gain1 * self.split_ratio()
+	}
+
+	pub fn split_gain2(&self) -> Float {
+		self.gain2 * self.split_ratio()
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_split_ratio() {
+		let row = HistoryRow {
+			year: 1970,
+			month: 1,
+			rent: 300.0,
+			gain1: 400.0,
+			gain2: 600.0,
+		};
+		assert_eq!(row.split_ratio(), 0.3);
+		assert_eq!(row.percentage(), 30.0);
+		assert_eq!(row.split_gain1(), 120.0);
+		assert_eq!(row.split_gain2(), 180.0);
 	}
 }
